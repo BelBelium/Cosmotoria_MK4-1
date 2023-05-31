d7b900df54cec444a2fcdafdff31d8a99cffb78e
Yoo-Yaho <dnjsldkqjwl@naver.com>
2023-06-01 01:58:03 +0900

    20230531 경계선, 오브젝트 충돌 다 추가
    일단 임시로 포탈 1, 2, 3 추가
    그리고 행성 리메이크

--- Assets/Scripts/StartIngame.cs
+++ Assets/Scripts/StartIngame.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public enum Number
+{
+    a, b, c
+}
+
 public class StartIngame : MonoBehaviour
 {
     public bool isTouch = false;
+
+    public Number number;
+    public string stageNumber;
+
+    private void Update()
+    {
+        stageNumber = number.ToString();
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -22,7 +35,24 @@ public class StartIngame : MonoBehaviour
 
     public void onClick()
     {
-        Loading.LoadScene("InGameScene");
-        GameManager.Instance.Stage = 33;
+        switch (stageNumber)
+        {
+            case "a":
+                GameManager.Instance.Stage = 11;
+                Loading.LoadScene("InGameScene");
+                Debug.Log("a 누름");
+                break;
+            case "b":
+                GameManager.Instance.Stage = 22;
+                Loading.LoadScene("InGameScene");
+                Debug.Log("b 누름");
+                break;
+            case "c":
+                GameManager.Instance.Stage = 33;
+                Loading.LoadScene("InGameScene");
+                Debug.Log("c 누름");
+                break;
+        }         
+        
     }
 }
