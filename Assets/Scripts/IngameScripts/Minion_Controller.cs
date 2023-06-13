2ef4454fc09b613ec5b2464d586d3e7e421e23df
KDH <ehdtls6211@gmail.com>
2023-06-14 01:14:50 +0900

    마지막 업데이트 완료.
    보스 하수인 파괴 이펙트 추가.

--- Assets/Scripts/IngameScripts/Minion_Controller.cs
+++ Assets/Scripts/IngameScripts/Minion_Controller.cs
@@ -8,6 +8,8 @@ public class Minion_Controller : MonoBehaviour
     private float ShootRate = 2.0f;
     public GameObject Bullet;
     public float Minion_HP = 10.0f;
+    public GameObject Des_Effect;
+    public GameObject I_Power;
 
     Coroutine MinionCor;
 
@@ -44,10 +46,29 @@ public class Minion_Controller : MonoBehaviour
             Minion_HP -= 1;
             if(Minion_HP <= 0)
             {
+                if (ItemDropManager.Drop_Percent(1.0f))
+                {
+                    DropItem();
+                }
                 StopCoroutine(MinionCor);
                 MinionCor = null;
+                DestroyEffect();
                 Destroy(gameObject);
             }
         }
+    }
+
+    void DropItem()
+    {
+        GameObject ins_Power = Instantiate(I_Power, transform.position, Quaternion.identity);
+        Rigidbody2D rb = ins_Power.GetComponent<Rigidbody2D>();
+        rb.AddForce(Vector3.up * 3.0f, ForceMode2D.Impulse);
+    }
+
+    public void DestroyEffect()
+    {
+        GameObject Des_Ins = Instantiate(Des_Effect, transform.position, Quaternion.identity);
+        Des_Ins.GetComponent<AudioSource>().Play();
+        Destroy(Des_Ins, 0.5f);
     }
 }
